6cbe5c651d1db60cb5485829c2738de873827662
juanjerezm <72557361+juanjerezm@users.noreply.github.com>
2021-12-13 14:18:47 -0300

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ Main author: Juan Jerez, jujmo@dtu.dk
 
 Configuration file of the Balmorel-HPC script. Here the user can define
 global settings that don't change across different projects in Balmorel.
-
 Thus, it should only be modified during installation or development.
 """
 
@@ -15,12 +14,9 @@ path_user = Path('/work3/jujmo/')
 # maximum number of simultaneous submissions to HPC, to avoid spamming the HPC
 max_runs = 10
 
-# if testing_mode is set to 'True', demo_project directories and data are used.
+# if option_testing is set to 'True', script uses example data from demo_project.
 # Default value for proper use is 'False'
-testing_mode = False
+option_testing = False
 
-# if option_submit is 'False', job submmission to the HPC is skipped
-# (submitting jobs in local machine breaks the code)
-# (can be mannually overriden if testing on the HPC)
-# option_submit = False if testing_mode else True
+# if option_submit is set to 'False', job submmission to the HPC is skipped (submitting jobs in the local machine breaks the code)
 option_submit = True
